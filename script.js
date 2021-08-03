5fe267a8c43e8df17a45b46b7ae4c865cc7bc9d7
Eduardo Seije <eduardoseije@gmail.com>
2021-08-03 10:14:33 -0300

    Create functions save, saveAndRender, createList, that will render the new task list and also save it to the user localStorage

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const listsContainer = document.querySelector('[data-lists]');
+const newListForm = document.querySelector('[data-new-list-form');
+const newListInput = document.querySelector('[data-new-list-input]');
 
-let lists = [{
-  id: 1,
-  name: 'name'
-}, {
-  id: 2,
-  name: 'todo'
-}];
+const LOCAL_STORAGE_LIST_KEY = 'task.lists';
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
+
+
+newListForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const listName = newListInput.value;
+  if (listName == null || listName === '') return;
+  const list = createList(listName);
+  newListInput.value = null;
+  lists.push(list);
+  saveAndRender();
+})
+
+function createList(name) {
+ return { id:Date.now().toString(), name: name, tasks: [] }
+}
 
 function render() {
   clearElement(listsContainer);
@@ -17,6 +29,15 @@ function render() {
     listElement.innerText = list.name;
     listsContainer.appendChild(listElement);
   })
+}
+
+function saveAndRender() {
+  save();
+  render();
+}
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY ,JSON.stringify(lists));
 }
 
 function clearElement(element) {
