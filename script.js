3f916ca23bee30b67ad19eb9f08994eac98eb0c5
Eduardo Seije <eduardoseije@gmail.com>
2021-08-03 10:26:49 -0300

    Add event listener to the delete list button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const listsContainer = document.querySelector('[data-lists]');
 const newListForm = document.querySelector('[data-new-list-form');
 const newListInput = document.querySelector('[data-new-list-input]');
+const deleteListButton = document.querySelector('[data-delete-list-button]')
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists';
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
@@ -13,6 +14,12 @@ listsContainer.addEventListener('click', e => {
     selectedListId = e.target.dataset.listId;
     saveAndRender();
   }
+})
+
+deleteListButton.addEventListener('click', e => {
+  lists = lists.filter(list => list.id !== selectedListId);
+  selectedListId = null;
+  saveAndRender();
 })
 
 newListForm.addEventListener('submit', e => {
